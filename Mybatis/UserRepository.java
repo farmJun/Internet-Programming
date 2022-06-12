0bcc389b88bb1fe20dd2caa755207e830ebb907e
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-06-12 22:22:53 +0900

    UserRepository
    UserRepository

---
+++ Mybatis/UserRepository.java
@@ -0,0 +1,71 @@
+package com.example.demo.repository;
+
+//package com.example.demo.repository;
+
+import com.example.demo.domain.User;
+import com.example.demo.util.DbUtil;
+
+import java.util.List;
+
+public class UserRepository{
+
+//    User findById(long id);
+//    void deleteById(Long id);
+	
+//	@Autowired
+//    private SqlSessionTemplate sqlSession;
+
+	public UserRepository() {
+		//connection = DbUtil.getConnection();
+		
+		//sqlSession = DbUtil.getSqlSession();
+		
+		
+	}
+	
+	
+	
+	
+	/*
+	 * return userRepository.findAll(); }
+	 * 
+	 * @Override public User findUserById(long id) { return
+	 * userRepository.findById(id); }
+	 * 
+	 * @Override public void save(User user) { userRepository.save(user); }
+	 * 
+	 * @Override public void edit(User user) { userRepository.save(user); }
+	 * 
+	 * @Override public void delete(long id) { userRepository.deleteById(id); }
+	 */
+	
+//	public List<User> findAll() {
+//		UserRepository mapper = sqlSession.getMapper(UserRepository.class);
+//		List<User> users = sqlSession.selectList("UserMapper.getAllUsers");
+//		return users;
+//	}
+	
+
+//	public void addUser(User user) {
+//		sqlSession.insert("UserMapper.addUser", user);
+//	}
+	
+	
+//	public void deleteUser(int userId) {
+//	sqlSession.delete("UserMapper.deleteUser", userId);
+//
+//}
+	
+	
+//	public void updateUser(User user) {
+//		sqlSession.update("UserMapper.updateUser", user);
+//	}
+//	
+//	
+//	public User getUserById(@Param("userId") int userId) {
+//		User user = sqlSession.selectOne("UserMapper.getUserById", userId);
+//		return user;
+//	}
+	
+	
+}
